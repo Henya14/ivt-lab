df95dfd2a2f6f16efca4170666019fc528fa4991
Henya14 <henrikeloe@gmail.com>
2021-04-15 16:15:46 +0200

    add additional comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -30,7 +30,7 @@ public class GT4500 implements SpaceShip {
   * 			- But if the store next in line is empty, the ship tries to fire the other store.
   * 			- If the fired store reports a failure, the ship does not try to fire the other one.
   * 	ALL:	tries to fire both of the torpedo stores.
-  *
+  *		Additional comment by B
   * @return whether at least one torpedo was fired successfully
   */
   @Override
