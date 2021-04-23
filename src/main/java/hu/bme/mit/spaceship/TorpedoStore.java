690ff6708db11145f0457ef9ab2a8f2205d44fb0
Henya14 <henrikeloe@gmail.com>
2021-04-23 20:57:38 +0200

    make random not static

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,7 +28,7 @@ public class TorpedoStore {
     }
   }
   
-  final Random generator = new Random();
+  public Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
