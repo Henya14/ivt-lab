078a72a9c92746965f228b3b940f25746ed19e4f
Henya14 <henrikeloe@gmail.com>
2021-04-23 20:53:42 +0200

    make random not public

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -28,7 +28,7 @@ public class TorpedoStore {
     }
   }
   
-  public static final Random generator = new Random();
+  final Random generator = new Random();
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
